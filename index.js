97b5358a2c1d3adeea3e483e6f9725e4e6676ce3
Ajinkya-commits <ajinkyathoke8585@gmail.com>
2026-03-10 02:33:44 +0530

    feat: add ci pipeline

---
+++ index.js
@@ -0,0 +1,2 @@
+console.log("Hello Devops");
+console.log("im learning cicd using github actions");
---
+++ test.js
@@ -0,0 +1,5 @@
+console.log("starting tests ...");
+setTimeout(() => {
+  console.log("running tests ...");
+}, 3000);
+console.log("tests completed");
